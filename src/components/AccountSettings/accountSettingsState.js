732b6fc222f56c2cf791d8a947d9e35c03667f6f
Vitor <vgvieira95@gmail.com>
2019-09-09 15:19:26 -0300

    feat: adding multiplication

--- src/components/AccountSettings/accountSettingsState.js
+++ src/components/AccountSettings/accountSettingsState.js
@@ -19,7 +19,7 @@ const AccountSettingsState = () => {
 
     const onBlur = () => {
         const { balance } = state;
-        const mutiplicator = balance < 150 ? 0.075 : balance > 150 && balance < 300 ? 0.65 : balance > 300 && balance < 500 ? 0.6 : balance > 500 && balance < 1000 ? 0.54 : 0.47;
+        const mutiplicator = balance < 150 ? 0.075 : balance > 150 && balance < 300 ? 0.065 : balance > 300 && balance < 500 ? 0.06 : balance > 500 && balance < 1000 ? 0.054 : 0.047;
         const bidValue = balance * mutiplicator > 0.35 ? balance * mutiplicator : 0.35;
         const stopWin = bidValue > 0.35 ? (((balance * mutiplicator) * 0.88) * 2) * 0.9 : 0.62;
         const stopLoss = (bidValue + (bidValue * 2) + (bidValue * 4) + (bidValue * 8)) * 0.8;
